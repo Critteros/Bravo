5258a8e85e1f6fc31184defdbadce08582014c9b
Critteros <szymonkoziol3@gmail.com>
2021-10-09 00:02:53 +0200

    working

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
+#include <sleepy_discord/sleepy_discord.h>
+
+#define SLEEPY_USE_HARD_CODED_GATEWAY
+
+class MyClientClass : public SleepyDiscord::DiscordClient
+{
+public:
+    using SleepyDiscord::DiscordClient::DiscordClient;
+    void onMessage(SleepyDiscord::Message message) override
+    {
+        if (message.startsWith("whcg hello"))
+            sendMessage(message.channelID, "Hello " + message.author.username);
+    }
+};
 
 int main()
 {
-    std::cout << "test adasd" << std::endl;
-    return 0;
+    MyClientClass client("test", SleepyDiscord::USER_CONTROLED_THREADS);
+    client.run();
 }
